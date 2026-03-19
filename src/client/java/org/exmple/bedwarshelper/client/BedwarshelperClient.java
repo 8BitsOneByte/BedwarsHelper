4b5138b40f3169dbc6573da56e221198510c14f9
8BitsOneByte <jone03085@gmail.com>
2026-03-19 10:33:23 +0800

    Initial commit

---
+++ src/client/java/org/exmple/bedwarshelper/client/BedwarshelperClient.java
@@ -0,0 +1,10 @@
+package org.exmple.bedwarshelper.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BedwarshelperClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/exmple/bedwarshelper/Bedwarshelper.java
@@ -0,0 +1,10 @@
+package org.exmple.bedwarshelper;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Bedwarshelper implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
